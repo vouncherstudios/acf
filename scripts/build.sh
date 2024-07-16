aeac86507eda69c34422a0eaf0f706b23fc5b09c
Luís Mendes <soconfirmo@hotmail.com>
2024-07-15 21:07:39 -0300

    Create workflow for publish

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,7 @@ git submodule update --init && ./scripts/applyPatches.sh || exit 1
 if [ "$1" == "--jar" ]; then
      cd ACF && mvn -B package --file pom.xml
 fi
+
+if [ "$1" == "--deploy" ]; then
+     cd ACF && mvn deploy
+fi
